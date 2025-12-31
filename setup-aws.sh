df57feca0e397e1c38f23d6d64e22bdffbe2a424
Erik Manor <erik.manor@okta.com>
2025-12-31 15:52:12 -0800

    Testing3

--- setup-aws.sh
+++ setup-aws.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 echo "--> Installing Docker"
-# sudo yum update -y
-# sudo yum install docker -y
+sudo yum update -y
+sudo yum install docker -y
 
 echo "--> Installing Docker-compose"
-# sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-# sudo chmod 755 /usr/local/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
 
 echo "--> configuring as a service"
-# sudo usermod -a -G docker ec2-user
-# sudo systemctl enable docker.service
+sudo usermod -a -G docker ec2-user
+sudo systemctl enable docker.service
 
 echo "--> Starting Docker"
-# sudo service docker start
+sudo service docker start
 
 echo "--> Installing ngrok"
-# wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-# sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 
 echo "--> Getting Configs"
 wget https://raw.githubusercontent.com/emanor-okta/go-scim/refs/heads/main/docker-compose.yml
 wget https://raw.githubusercontent.com/emanor-okta/go-scim/refs/heads/main/config.yaml
 
 echo "--> Creating Volume setting scripts"
-# docker volume create goscim-config
+docker volume create goscim-config
 goscim_config_destination=`docker volume inspect goscim-config | grep Mountpoint | awk -F'"' '{print $4}'`
-# sudo cp config.yaml $goscim_config_destination
+sudo cp config.yaml $goscim_config_destination
 echo "alias vigoscim2='sudo vi ${goscim_config_destination}/config.yaml'" >> ~/.bash_profile
 
 echo "--> done.."
